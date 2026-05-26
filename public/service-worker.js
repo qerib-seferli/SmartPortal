49dfa941b4f50e9d82525c622de9383ba4107eec
qerib-seferli <seferovqerib09@gmail.com>
2026-05-27 02:14:57 +0400

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Bu service worker GLOBAL PRO PWA shell, portal və admin fayllarını offline cache üçün saxlayır.
-const CACHE = "smartportal-global-pro-v1";
+const CACHE = "smartportal-global-pro-v2";
 const FILES = [
   "../index.html",
   "../admin.html",
